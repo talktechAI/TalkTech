ed558a070fc8cd5e0b96b2af5f6f86a145bfba66
ScottS <talktech325@gmail.com>
2025-08-29 16:38:51 -0400

    email update

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -71,7 +71,7 @@ export async function POST(request: NextRequest) {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          from: 'noreply@yourdomain.com',
+          from: 'noreply@talktech.io',
           to: env.NOTIFICATION_EMAIL,
           subject: `New Contact Form Submission from ${body.name}`,
           html: `
