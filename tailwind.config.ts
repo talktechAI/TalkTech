da69eea834757a0c0a87c524eb6e4da1d1b8bfaf
ScottS <talktech325@gmail.com>
2025-09-02 20:37:05 -0400

    Delete tailwind.config.ts
    Signed-off-by: ScottS <talktech325@gmail.com>

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-
-import type { Config } from "tailwindcss"
-const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
-  darkMode: ["class", "[data-theme='dark']"],
-  plugins: [],
-}
-export default config
